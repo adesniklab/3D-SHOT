23b808de563ffcb9f96a86350c07e544f4137649
nicopegard <nicopegard@gmail.com>
2018-04-27 11:04:58 -0700

    Added standalone NovoCGH control

---
+++ NOVOCGH_Code/function_Superoposition.m
@@ -0,0 +1,18 @@
+function [ Superposition ] = function_Superoposition( System,HStacks,Masks )
+%This algorithm conducts a simple superposition 
+if System.verbose == 1; fprintf('Superposition hologram computation begins...'); tic; end;
+[NX,NY,LZ] = size(Masks);
+hologram = zeros(NX, NY);
+for i = 1:LZ
+    target = max(Masks(:,:,i),0);
+    hologram = hologram + ifft2(ifftshift(sqrt(target) .* exp(1i * randn(NX,NY))))./HStacks(:,:,i);
+end
+if System.verbose == 1
+t = toc;
+Superposition.hologram = hologram;
+Superposition.phase = gather(angle(Superposition.hologram));
+
+disp(['Completed in ' int2str(t) ' seconds !']);
+end
+end
+
---
+++ NOVOCGH_Code/function_dummydata.m
@@ -0,0 +1,45 @@
+function [ ims, darkims, imdepths ] = function_dummydata( Setup )
+%%% This function for the purposes only of exemple simulation, provides a
+%%% series of target intensity (ims), dark voxels (darkims), and depths (imdepths) for the desired format. 
+
+load('Dummy_Data');
+imdepths =0.1*linspace(1,10,10);
+masks = masks(220:380,325:485,:);
+ims = imresize(masks, [Setup.Nx, Setup.Ny]);
+
+if Setup.verbose == 1;
+disp('Dummy data Ready !')
+end
+
+ims = double(ims>0.3);
+
+v = squeeze(sum(sum(double(ims>0.1),1),2)); v = v/(max(v)); v = 1./v;
+for i = 1:10
+    pick = squeeze(ims(:,:,i)); 
+    pick = imresize(pick, sqrt(v(i)));
+    [cx cy] = size(pick);
+    pick = pick(floor(cx/2-Setup.Nx/2+1):floor(cx/2+Setup.Nx/2),floor(cy/2-Setup.Ny/2+1):floor(cy/2+Setup.Ny/2));
+        pause(0.01)
+ ims(:,:,i) =pick;   
+end
+ims = double(ims>0.1);
+v = squeeze(sum(sum(double(ims>0),1),2)); 
+
+
+LN = 5;
+ims = ims(:,:,1:LN);
+imdepths = imdepths(1:LN);
+imdepths =imdepths -mean(imdepths);
+
+darkims = 40*ims;
+ims(:,:,2) = 0;
+ims(:,:,4) = 0;
+darkims(:,:,1) = 0;
+darkims(:,:,3) = 0;
+darkims(:,:,5) = 0;
+
+%darkims = darkims-0.01;
+
+
+end
+
